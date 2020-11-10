437ae81e4088f067d53cdfc36fb2c05771725c17
Rishabh Tripathi <rishtrip.23@gmail.com>
2020-11-10 19:36:45 +0530

    Update CuckooSearch.py

--- CuckooSearch/CuckooSearch.py
+++ CuckooSearch/CuckooSearch.py
@@ -3,8 +3,10 @@ import numpy as np
 import random, copy, json
 import matplotlib.pyplot as plt
 
+#Creating a class that consists of the main evolutionary process of the search.
 class CuckooSearch:
 
+#The constructor intializes the main parameters that are feeded through the config file and initialize the solution matrix.
     def __init__(self):
         loader = json.load(open("config.json", "r"))
         self.P_a = loader["P_a"]
@@ -18,6 +20,7 @@ class CuckooSearch:
         self.matrix = self.lb + np.random.rand(self.nests, self.dim) * (self.ub - self.lb)
         print(self.matrix)
 
+#Computes fitness values for the values of the x's i.e for all the initialized nests, the functional value is computed.
     def InitializeCandidateSoln(self):
         matrix = self.matrix.T
         x1, x2 = matrix[0], matrix[1]
@@ -26,6 +29,7 @@ class CuckooSearch:
         self.matrix = np.concatenate([self.matrix, np.array([fitness_value]).T], axis=-1)
         return self.matrix, matrix          # matrix in shape(nest,dim), matrix in shape(dim, nest)
 
+#This Function is the most important function that scopes and traces the search of the cuckoo bird for the optimal Nest.
     def Search(self):
         best_soln = None
         mat_nest_dim, mat_dim_nest = self.InitializeCandidateSoln()
@@ -34,6 +38,7 @@ class CuckooSearch:
         store_individual_candidate = []
 
         for i in range(self.max_generations):
+            #Best solution is the nest for which the value of the function is the least(OPTIMAL).
             best_index = np.argmin(mat_dim_nest[-1])
             best_soln = mat_nest_dim[best_index]
             best_soln, best_fitness_val = best_soln[:-1], best_soln[-1]
@@ -47,7 +52,8 @@ class CuckooSearch:
             # updating the pre-existing solution with new solution if new solution is better than pre-existing solution
             for index, new_pos, new_fitness in zip(nest_indices, new_cuckoo_pos, new_fitness_val):
                 old_fitness = mat_nest_dim[index][-1]
-                if(new_fitness < old_fitness):       # greedy approach
+                #GREEDY APPROACH:If the newer fitness better then its updated.
+                if(new_fitness < old_fitness):
                     mat_nest_dim[index] = np.concatenate([new_pos, [new_fitness]], axis=0)
 
             # abandon the nests with P_a probability and build newer ones
@@ -67,12 +73,12 @@ class CuckooSearch:
             fitness_vector = mat_nest_dim.transpose()[-1]
             store_individual_candidate.append(fitness_vector)
         store_individual_candidate = np.array(store_individual_candidate).transpose()  # matrix of 5x20, each nest solution over 20 generations.
-
+        #plotting a graph of the no of Generation(Iterations) VS The Sum of the Fitness Value of all the nests
         plt.figure()
         plt.plot(store_sum_candidate, list(range(self.max_generations)))
         plt.xlabel("Generations/Iterations")
         plt.ylabel("Sum of Fitness_Values for all nests")
-
+        #Plotting a graph of the No of Generations VS The Fitness Value for each value in each generation.
         plt.figure()
         plt.xlabel("Generations/Iterations")
         plt.ylabel("Fitness_Value")
